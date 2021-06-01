be0fc80c4354fbc3091bd80933e47381300e6543
chris <cseiter@gmail.com>
2021-05-31 22:23:34 -0400

    actor names displaying through component

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,15 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
+  const indActor = actors.map((actor) => (
+    <div key={actor.name}>
+      <h2>{actor.name}</h2>
+    </div>
+  ));
   return (
   <div>
     <h1>Actors Page</h1>
+    {indActor}
   </div>
   );
 }
