67b44d8d935ecb9f3ec3b08bfdc3abc1053fc940
chris <cseiter@gmail.com>
2021-05-31 22:09:16 -0400

    directors displayed through directors component

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,15 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
+  const indDirector = directors.map((director) => (
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+    </div>
+  ));
 return (
 <div>
   <h1>Directors Page</h1>
+  {indDirector}
 </div>
 )
 }
