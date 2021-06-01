52d39baa048e090c404dce02504feb44b06946f1
chris <cseiter@gmail.com>
2021-05-31 21:46:34 -0400

    Movie titles displayed through movies component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,15 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
+  const indMovie = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+    </div>
+  ));
   return (
     <div>
       <h1> Movies Page</h1>
+      {indMovie}
     </div>
   );
 }
